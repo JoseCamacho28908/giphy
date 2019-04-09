5296233137e8a90f72fd0a2632aa22521f310311
Jose Camacho <josecamacho@students.ptechnyc.org>
2019-04-09 21:57:13 +0000

    When button clicked, it will display a gif

--- js/script.js
+++ js/script.js
@@ -3,9 +3,18 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
-$("#search-button").click(function(){
-  
-  
-  
-});
+$("#search-button").click(function() {
+    var user_search = $("input").val();
 
+    var request_url = "https://api.giphy.com/v1/gifs/search?q=" + user_search + "&rating=pg&api_key=dc6zaTOxFJmzC";
+
+    $.ajax({
+        url: request_url,
+        method: "GET",
+        success: function(response) {
+            var pic_url = response.data[0].images.original.url;
+            $("#image_display").append("<img src=" + pic_url + ">");
+        }
+    });
+
+});
