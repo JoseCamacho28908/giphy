2020c486b4f23502fab2011116f553c1dc0d341a
Jose Camacho <josecamacho@students.ptechnyc.org>
2019-04-16 21:14:51 +0000

    Added math.random to display one random gif

--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@
 
 $("#search-button").click(function() {
     var user_search = $("input").val();
-
+    var new_number= Math.floor(Math.random()*100);
     var request_url = "https://api.giphy.com/v1/gifs/search?q=" + user_search + "&rating=pg&api_key=dc6zaTOxFJmzC";
 
     $.ajax({
         url: request_url,
         method: "GET",
         success: function(response) {
-            var pic_url = response.data[0].images.original.url;
-            $("#image_display").append("<img src=" + pic_url + ">");
+            var pic_url = response.data[new_number].images.original.url;
+            $("#image_display").html("<img src=" + pic_url + ">");
         }
     });
 
